8069c1b5605f688157a3f36dacd774f07124305e
Hanguyen2332 <thanhha2332@gmail.com>
2021-06-19 13:18:32 +0700

    correcting_practice_3

--- src/_11_stack_queue/practice/th3/MyQueue.java
+++ src/_11_stack_queue/practice/th3/MyQueue.java
@@ -1,12 +1,10 @@
 package _11_stack_queue.practice.th3;
 
-import java.util.Queue;
-
 public class MyQueue {
     private int capacity;
     private int[] queueArr;
-    private int head;
-    private int tail;
+    private int head = 0;  //head luôn trỏ sẵn đến vị trí index = 0 (đầu mảng)==>khi dequeue: head++ =1 --> giá trị đầu mảng (tại index = 0) bị pop ra ngoài.
+    private int tail = -1;  // để khi thêm phần tử mới --> tail ++ = 0 --> index đầu tiên đc gán giá trị (NOTE: chiều thêm: đi tù cuối mảng đến)
     private int currentSize;
 
     public MyQueue(int queueSize) {
@@ -27,29 +25,54 @@ public class MyQueue {
         }
         return false;
     }
-    public void enqueue(int item) throws Exception { //thêm cuối
+
+    public void enqueue(int item) throws Exception { //thêm: chiều thêm từ cuối vào
         if (this.isQueueFull()) {
             throw new Exception("Queue is FULL! Can NOT enqueue!");
         } else {
-            //tăng con trỏ tail lên --> trỏ tới ô trống tiếp theo,gán giá trị
-            tail = tail + 1;
+            /*KIỂM TRA ĐIỀU KIỆN XOAY VÒNG (liên tưởng Mảng vòng tròn):
+            sau khi check if bên trên --> mảng chưa Full, nhưng tail đã chỉ đến vị trí index = length - 1 ==> không thể thêm mới,mặc dù phía đầu mảng trống.
+            nguyên nhân: do sau QT thêm/bớt --> tail bị đẩy dần đến bờ vực thẳm :))
+            --> cần check ĐK xoay vòng, gán cho tail = 0 --> tận dụng ô trống, tiếp tục gán giá trị mới:
+             */
+            if (tail == capacity - 1) {
+                tail = 0;
+            } else {
+                //nếu không --> tăng con trỏ tail lên --> trỏ tới ô trống tiếp theo
+                tail = tail + 1;
+            }
+            //gán giá trị
             queueArr[tail] = item;
             //tăng size
             currentSize++;
-            System.out.println("add Successfully! added: " + queueArr[tail]);
+            System.out.println("Added: " + queueArr[tail] + ", index = " + tail);
         }
     }
 
     public void dequeue() throws Exception { //lấy đầu
+        int removeData;
+        int index;
         if (this.isQueueEmpty()) {
             throw new Exception("Queue is EMPTY! Can NOT dequeue!");
         } else {
-            //tăng con trỏ head lên 1 đơn vị --> chỉ tới phần tử phía sau
-            head++;
-            int removeData = queueArr[head];
+            /*KIỂM TRA ĐIỀU KIỆN XOAY VÒNG: head đã tới vị trí length - 1, nhưng đầu mảng vẫn có phần tử
+            do: tail thêm vào sau (bên trên)
+            --> không tiếp tục dequeue được, dù vẫn còn phần tử (nếu tiếp tục head++ --> head >= length --> lỗi index vượt mảng)
+            --> cần check, set lại head về 0
+             */
+            if (head == capacity - 1) {
+                head = 0;
+                removeData = queueArr[capacity - 1];
+                index = capacity - 1;
+            } else {
+                //nêu không: tăng con trỏ head lên 1 đơn vị --> chỉ tới phần tử phía sau, loại phần tử trước
+                head++;
+                removeData = queueArr[head - 1]; //giá trị head vừa trỏ vào trước đó
+                index = head - 1;
+            }
             //giảm size
             currentSize--;
-            System.out.println("dequeue Successfully. removed: " + removeData);
+            System.out.println("Removed: " + removeData + ", index = " + index);
         }
     }
 }
